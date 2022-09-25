cc1ecc70c9c2217e4ad6ff017edffc8f5c93d8c5
Evgeniy Sharychenkov <sanctio@yandex.ru>
2022-09-25 19:16:56 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,20 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        double x1;
+        double x2;
 
+        double dis = Math.pow(b,2) - 4 * a * c;
 
-
+        if (dis > 0) {
+            x1 = (-b + Math.sqrt(dis)) / (2 * a);
+            x2 = (-b - Math.sqrt(dis)) / (2 * a);
+            System.out.println(x1 + " " + x2);
+        } else if (dis == 0) {
+            x1 = -(b / 2 * a);
+            System.out.println(x1);
+        } else {
+            System.out.println("no roots");
+        }
     }
-
 }
